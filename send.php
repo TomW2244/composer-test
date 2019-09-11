9342d0fe644d4345883d54034e0a0bcf47eb6e45
TomWright <tomwright24.3@icloud.com>
2019-09-11 15:14:50 +0100

    setting up server.ini

--- send.php
+++ send.php
@@ -4,14 +4,13 @@ require_once "vendor/autoload.php";
 use League\Csv\Reader;
 use League\Csv\Statement;
 
+$config = parse_ini_file("server.ini");
 $csv = Reader::createFromPath('MOCK_DATA.csv', 'r');
 $csv->setHeaderOffset(0); //set the CSV header offset
 
 // setup request
 $client = new GuzzleHttp\Client();
-$request = new \GuzzleHttp\Psr7\Request('GET', 'http://requestbin.net/r/117jch31');
 
-//get 25 records starting from the 11th row
 $stmt = (new Statement())
     ->offset(0)
     ->limit(10)
@@ -22,19 +21,11 @@ $records = $stmt->process($csv);
 
 
 foreach ($records as $record) {
-    $r = $client->request('POST', 'http://requestbin.net/r/117jch31', [
+    $r = $client->request('POST', $config['url'], [
     'body' => json_encode($record)
 ]);
     print_r($r->message);
 }
-
-
-
-
-
-
-// Send an asynchronous request.
-
 
 
 
